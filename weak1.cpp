c66e6ce320e37a1abd1c58fd8e035402ac69c285
rock <rock951228@gmail.com>
2023-12-02 10:06:31 +0800

    Add weak1.cpp

---
+++ weak1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 變數宣告
+    int number = 10;
+
+    // 陣列宣告
+    int array[5] = {1, 2, 3, 4, 5};
+
+    // 運算符
+    int result = number * array[2];
+
+    // 輸出結果
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
